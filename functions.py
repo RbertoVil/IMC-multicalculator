313962832760d926b5960ec369d16378ff3838f1
RbertoVil <rantoniovillasana@gmail.com>
2022-07-19 19:26:23 +0000

    Add IMC function in functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+def IMC(nombrePersona, peso, estatura):
+	# Determinar el IMC de una persona
+	denominador = estatura ** 2
+	resultadoIMC = peso / denominador
+
+	if resultadoIMC <= 18.5:
+		composicionCorporal = "Peso inferior al normal."
+	elif resultadoIMC > 18.5 and resultadoIMC <= 24.9:
+		composicionCorporal = "Peso normal."
+	elif resultadoIMC > 24.9 and resultadoIMC <= 29.9:
+		composicionCorporal = "Peso superior al normal."
+	else:
+		composicionCorporal = "Obesidad."
+
+	return(resultadoIMC, composicionCorporal)
