243301bf1db54d67048e35400a09467add1d3530
RbertoVil <rantoniovillasana@gmail.com>
2022-07-20 02:24:16 -0400

    add function results

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import functions
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/results", methods=["POST","GET"])
+def results():
+    nombre = request.form.get("name")
+    peso = request.form.get("weight")
+    estatura = request.form.get("height")
+
+    aplicarIMC = functions.IMC(nombre, peso, estatura)
+    resultadoIMC = aplicarIMC[0]
+    composicionCorporal = aplicarIMC[1]
+    return
+
