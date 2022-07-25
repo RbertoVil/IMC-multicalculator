8076a4318112cf25f2801f131ac70d7d474091a4
RbertoVil <rantoniovillasana@gmail.com>
2022-07-24 20:22:02 -0400

    Test importing classes

--- scriptForTestings.py
+++ scriptForTestings.py
@@ -1,3 +1,12 @@
 import functions
 
-functions.crearPDF(2)
+def pruebaClases():
+    estudiante = functions.persona()
+
+    estudiante.id = "estudiante1"
+    estudiante.name = "Roberto"
+    estudiante.peso = 70
+    estudiante.estatura = 1.75
+    print(estudiante)
+
+#functions.crearPDF(2)
